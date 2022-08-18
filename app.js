54aff2ec391a1155c4ef97bae766a60a1bd8f06a
Joshua Henson <tktjoshuahenson@yahoo.com>
2022-08-18 16:09:10 -0700

    added console table

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
 // get the client
 const mysql = require('mysql2');
+const cTable = require('console.table');
+
+
+
+
+
+
+console.table([
+    {
+      name: 'foo',
+      age: 10
+    }, {
+      name: 'bar',
+      age: 20
+    }
+  ]);
+  
+  // prints
+  name  age
+  ----  ---
+  foo   10
+  bar   20
+
+
+
+
+
+
 
 // create the connection to database
 const connection = mysql.createConnection({
