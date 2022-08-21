0d86a5b76e99db25bdf3bd9b5c67d8abd1b1f5c4
BoiledLettuce <tktjoshuahenson@yahoo.com>
2022-08-20 17:20:36 -0700

    added functions

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 // get the client
 const mysql = require('mysql2');
+
 const cTable = require('console.table');
 const inquirer = require('inquirer');
 const { default: Choices } = require('inquirer/lib/objects/choices');
 
 const path = require('path');
 const express = require('express');
+const { start } = require('repl');
 
 //DATABASE CONNECTION
 const connection = mysql.createConnection({
@@ -23,6 +25,12 @@ connection.connect(err => {
   menu();
 });
 
+
+
+
+// FUNCTIONS
+
+// MENU FUNCTION
 function menu() {
   inquirer.prompt({
     name: "menu",
@@ -43,28 +51,54 @@ function menu() {
       case "View roles": viewRoles(); break;
       case "View departments": viewDepartments(); break;
       case "Add employee": addEmployee(); break;
+      case "Add department": addDepartment(); break;
       case "Exit": connection.destroy(); break;
     }
   });
 }
+// MENU FUNCTION
+
+
 
 function viewEmployees() {
 
 
-  
+
 }
 
 function viewRoles() {
-
-
-
+  connection.query("SELECT * FROM role", (err, data) => {
+    if (err) throw err;
+    menu();
+  });
 }
 
+
+
+
+// DEPARTMENT
 function viewDepartments() {
-
+  connection.query("SELECT * FROM deparment", (err, data) => {
+    if (err) throw err;
+    console.table(data);
+    menu();
+  });
 
 
 }
+
+function addDepartment() {
+  inquirer.prompt([
+    {
+      name: "department",
+      type: "input",
+    }
+  ]).then
+}
+// DEPARTMENT
+
+
+
 
 function addEmployee() {
 
